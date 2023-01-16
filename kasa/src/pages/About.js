bbb1a7a98856191abb6dd8c907f29f10b277f55c
AlexandreSZ <contact.alexandre.szumiel@gmail.com>
2023-01-16 10:14:15 +0100

    page error404 +css

--- kasa/src/pages/About.js
+++ kasa/src/pages/About.js
@@ -3,6 +3,7 @@ import Navigation from '../components/Navigation'
 import Banner from '../components/Banner'
 import aboutImg from '../assets/images/ImgBannerAbout.png'
 import Dropdown from '../components/Dropdown'
+import Footer from '../components/Footer'
 
 const About = () => {
   return (
@@ -27,6 +28,7 @@ const About = () => {
           text="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
         />
       </section>
+      <Footer />
     </div>
   )
 }
--- kasa/src/pages/Error.js
+++ kasa/src/pages/Error.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Navigation from '../components/Navigation'
 
 const Erreur = () => {
   return (
     <div>
-      <h1>404 not found</h1>
+      <Navigation />
+      <section className="page404Section">
+        <h7>404</h7>
+        <p>Oups! La page que vous demandez n'existe pas.</p>
+        <Link to="/" className="backHomeLink">
+          Retourner sur la page d'accueil
+        </Link>
+      </section>
     </div>
   )
 }
