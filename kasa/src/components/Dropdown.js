ec7a015943c229c99858707988c282649edc121e
AlexandreSZ <contact.alexandre.szumiel@gmail.com>
2023-01-18 21:36:30 +0100

    modif dropdown + implentation dans logement

--- kasa/src/components/Dropdown.js
+++ kasa/src/components/Dropdown.js
@@ -1,43 +1,22 @@
 import React from 'react'
 import arrowDown from '../assets/images/Vectordown.png'
-import arrowUp from '../assets/images/Vector.png'
-import { useState } from 'react'
 
-const Dropdown = ({ title, text, extraClass }) => {
-  const [dropdownOpened, setDropdown] = useState(false)
-
-  return dropdownOpened ? (
-    <div className={extraClass ? 'dropdown' + extraClass : 'dropdown'}>
-      <div className="dropdown_header">
-        <h3>{title}</h3>
-        <img
-          src={arrowUp}
-          alt="Cacher contenu"
-          onClick={() => setDropdown(false)}
-        />
-      </div>
-      {typeof text === 'object' ? convertArray(text) : <p>{text}</p>}
-    </div>
-  ) : (
-    <div className="dropdown">
-      <div className="dropdown_header">
-        <h3>{title}</h3>
-        <img
-          src={arrowDown}
-          alt="Afficher contenu"
-          onClick={() => setDropdown(true)}
-        />
-      </div>
-    </div>
-  )
-}
-function convertArray(arr) {
+function Dropdown({ title, content, details, summary, text }) {
   return (
-    <ul>
-      {arr.map((equipment) => (
-        <li>{equipment}</li>
-      ))}
-    </ul>
+    <details className={details}>
+      <summary className={summary}>
+        {title} <img src={arrowDown} className="chevron-down" alt=""></img>
+      </summary>
+      {typeof content === 'object' ? (
+        <ul className={text}>
+          {content.map((element) => {
+            return <li key={`${element}`}> {element}</li>
+          })}
+        </ul>
+      ) : (
+        <p className={text}>{content}</p>
+      )}
+    </details>
   )
 }
 
--- kasa/src/pages/logements.js
+++ kasa/src/pages/logements.js
@@ -1,17 +1,31 @@
 import React from 'react'
-
 import { useParams } from 'react-router-dom'
 import Dropdown from '../components/Dropdown'
 import Navigation from '../components/Navigation'
 import Slider from '../components/Slider'
+import Error from './Error'
 
 const Logements = ({ data }) => {
   const { id } = useParams()
-  const items = data.filter((item) => item.id === id)
+  const dataDetails = data.find((item) => item.id === id)
+  if (!dataDetails) {
+    return <Error />
+  }
+  const {
+    title,
+    location,
+    tags,
+    host,
+    rating,
+    description,
+    equipments,
+  } = dataDetails
   return (
     <div>
       <Navigation />
       <Slider />
+      <Dropdown title="Equipements" content={equipments} />
+      <Dropdown title="Description" content={description} />
     </div>
   )
 }
