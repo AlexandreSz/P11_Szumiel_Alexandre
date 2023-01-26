cd38c21df62604e8ba445fce0b5c6e0e1eef8e8f
AlexandreSZ <contact.alexandre.szumiel@gmail.com>
2023-01-26 02:26:34 +0100

    modif scss page error

--- kasa/src/routes/App.js
+++ kasa/src/routes/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import data from './data/data.json'
+import data from '../data/data.json'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import About from './pages/About'
-import Erreur from './pages/Error'
-import Home from './pages/Home'
-import Logement from './pages/Logements'
+import About from '../pages/About'
+import Erreur from '../pages/Error'
+import Home from '../pages/Home'
+import Logement from '../pages/Logements'
 
 const App = () => {
   return (
